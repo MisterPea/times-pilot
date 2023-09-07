919aea76a47c1eaef6bba5d52b1b100e4ea08f3a
MisterPea <pea.mail.list@gmail.com>
2023-09-07 12:54:47 -0400

    Initial setup - removed defaults

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import "../src/style/global.scss"
+import Head from "next/head";
+
+export default function MainApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>the.times.pilot</title>
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, viewport-fit=cover" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
