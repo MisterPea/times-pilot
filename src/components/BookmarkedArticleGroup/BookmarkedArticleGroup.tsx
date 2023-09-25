b2e1e5428934d830c44d9f650012d29fcf6f7056
MisterPea <pea.mail.list@gmail.com>
2023-09-24 21:09:53 -0400

    Initial commit

---
+++ src/components/BookmarkedArticleGroup/BookmarkedArticleGroup.tsx
@@ -0,0 +1,58 @@
+import { useState, useRef, useEffect } from "react";
+import BookmarkedArticle from "../BookmarkedArticle/BookmarkedArticle";
+import Label from "../Label/Label";
+
+type Bookmark = {
+  id: string,
+  date: string,
+  title: string,
+  summary: string,
+  link: string,
+};
+
+interface BookmarkGroupProps {
+  bookmarks: Bookmark[];
+}
+export default function BookmarkedArticleGroup({ bookmarks }: BookmarkGroupProps) {
+  const [localBookmarks, setLocalBookmarks] = useState<Bookmark[]>(bookmarks);
+  const bookmarksRef = useRef<HTMLDivElement | null>(null);
+
+  function deleteListItem(id: string) {
+    const bkmk_group = document.querySelector('.bookmark_group');
+    const toRemove: HTMLLIElement | undefined | null = bkmk_group?.querySelector(`#${id}`);
+    if (toRemove) {
+      toRemove.style.maxHeight = "0px";
+      toRemove.style.margin = "-1px 0";
+      toRemove.addEventListener('transitionend', () => {
+        setLocalBookmarks((s) => s.filter((elem) => elem.id !== id));
+      }, { once: true });
+    }
+  }
+
+  function parseDate(dateString: string) {
+    const monthAbbreviations = [
+      "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+      "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"
+    ];
+    const parsedDate = new Date(dateString);
+    const year = parsedDate.getFullYear();
+    const month = parsedDate.getMonth();
+    const day = parsedDate.getDate();
+    const monthAbbreviation = monthAbbreviations[month];
+    const formattedDate = `${monthAbbreviation} ${day.toString().padStart(2, '0')} ${year}`;
+    return formattedDate
+  }
+
+  return (
+    <div ref={bookmarksRef} className="bookmark_group">
+      {localBookmarks.length === 0 && <div className="no_bookmarks"><Label label="You have no bookmarked articles." size="sm" /></div>}
+      <ul>
+        {localBookmarks.map(({ id, date, title, summary, link }) => (
+          <li id={id} className="bookmark_group--element" key={id}>
+            <BookmarkedArticle date={parseDate(date)} headline={title} summary={summary} id={id} linkToArticle={link} deleteCallback={deleteListItem} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/stories/BookmarkedArticlesGroup.stories.ts
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import BookmarkedArticleGroup from "../components/BookmarkedArticleGroup/BookmarkedArticleGroup";
+import '../style/global.scss';
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+
+const meta: Meta<typeof BookmarkedArticleGroup> = {
+  title: 'Article/Bookmarked Group',
+  component: BookmarkedArticleGroup,
+  parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewport: 'iphone12'
+    },
+    layout: 'fullscreen'
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof BookmarkedArticleGroup>;
+
+const bookmarks = [
+  {
+    id: "x1abcd",
+    date: "2023-09-18T15:27:12-04:00",
+    title: "The Senate Dress Code Gets a Casual Overhaul",
+    summary: "Senator Chuck Schumer, the majority leader, will no longer enforce the informal dress code for the chamber, which for decades has dictated that members wear business attire.",
+    link: "https://www.nytimes.com/2023/09/18/us/politics/senate-dress-code.html",
+  },
+  {
+    id: "x2defg",
+    date: "2023-09-18T15:00:07-04:00",
+    title: "Bear Sighting Forces Closures at Disney World",
+    summary: "The black bear, which was spotted in a tree, was captured on Monday after some rides and attractions in the Magic Kingdom were closed.",
+    link: "https://www.nytimes.com/2023/09/18/us/disney-world-bear-magic-kingdom.html",
+  },
+  {
+    id: "x3lmnop",
+    date: "2023-09-18T15:00:07-04:00",
+    title: "I.R.S. Changes Audit Practice That Discriminated Against Black Taxpayers",
+    summary: "The agency will overhaul how it scrutinizes returns that claim the earned-income tax credit, which is aimed at alleviating poverty.",
+    link: "https://www.nytimes.com/2023/09/18/us/politics/irs-audits-black-taxpayers.html",
+  },
+  {
+    id: "x4klnm",
+    date: "2023-09-18T09:08:49-04:00",
+    title: "With a New Formula, U.S. News Rankings Boost Some State Universities",
+    summary: "Responding to critics, the publication made its biggest changes to its method in four decades. But it's unclear whether that is enough to quiet the naysayers.",
+    link: "https://www.nytimes.com/2023/09/18/us/us-news-college-ranking.html",
+  }
+];
+
+
+export const Primary: Story = {
+  args: {
+    bookmarks
+  }
+}
+
