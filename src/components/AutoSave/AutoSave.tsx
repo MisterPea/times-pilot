f85b544daddb6791a7f8694394636bcab1e39510
MisterPea <pea.mail.list@gmail.com>
2023-10-07 00:14:54 -0400

    Revised and simplified auto save css/jsx

--- src/components/AutoSave/AutoSave.tsx
+++ src/components/AutoSave/AutoSave.tsx
@@ -6,52 +6,29 @@ interface AutoSaveProps {
 }
 
 export default function AutoSave({ saving, error }: AutoSaveProps) {
-  const innerWrapRef = useRef<HTMLUListElement | null>(null);
-  const isSavedRef = useRef<HTMLLIElement | null>(null);
-  const savingRef = useRef<HTMLLIElement | null>(null);
-  const errorRef = useRef<HTMLLIElement | null>(null);
 
-  useEffect(() => {
-    if (saving === true) {
-      const savingTop = savingRef.current?.getBoundingClientRect().top;
-      savingTop && updateMessage(savingTop, savingRef.current);
-    } else if (error === true) {
-      const errorTop = errorRef.current?.getBoundingClientRect().top;
-      errorTop && updateMessage(errorTop, errorRef.current);
-    } else {
-      const isSavedTop = isSavedRef.current?.getBoundingClientRect().top;
-      isSavedTop && updateMessage(isSavedTop, isSavedRef.current);
+  function combineClass() {
+    const baseClass = ['auto_save_inner_wrapper'];
+    if (saving) {
+      baseClass.push('is_saving');
     }
-  }, [saving, error]);
-
-
-  function updateMessage(messageTop: number, reference: HTMLLIElement | null) {
-    innerWrapRef.current?.querySelectorAll('.condition').forEach((e) => {
-      if (e.classList !== reference?.classList) {
-        if (e instanceof HTMLElement) { // cast e as HTMLElement
-          e.style.opacity = "0";
-          e.setAttribute("aria-hidden", "true");
-        }
-      } else {
-        if (e instanceof HTMLElement) {
-          e.style.opacity = "1";
-          e.setAttribute("aria-hidden", "false");
-        }
-      }
-    });
-
-    const wrapTop = innerWrapRef.current?.getBoundingClientRect().top;
-    const newTop = wrapTop && wrapTop - messageTop;
-    innerWrapRef.current!.style.top = `${newTop}px`;
-  }
+    else if (error) {
+      baseClass.push('error');
+    }
+    else {
+      baseClass.push('saved');
+    }
+    return baseClass.join(' ');
+  } 
 
   return (
     <div className="auto_save_base">
       <div className="auto_save_inner" aria-live="polite">
-        <ul ref={innerWrapRef} className="auto_save_inner_wrapper">
-          <li ref={isSavedRef} className="condition is-saved">• All Changes Saved</li>
-          <li ref={savingRef} className="condition saving">• Saving...</li>
-          <li ref={errorRef} className="condition error">• Changes Not Saved!</li>
+        <ul
+          className={combineClass()}>
+          <li className={`condition saved ${!saving && !error ? "active" : ""}`}>• All Changes Saved</li>
+          <li className={`condition is_saving ${saving ? "active" : ""}`}>• Saving...</li>
+          <li className={`condition error ${error ? "active" : ""}`}>• Changes Not Saved!</li>
         </ul>
       </div>
     </div>
