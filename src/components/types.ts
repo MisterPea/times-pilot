f10af60dcd91185dda71d6a7380e07e8109713e1
MisterPea <pea.mail.list@gmail.com>
2023-11-01 23:20:09 -0400

    Initial commit

---
+++ src/components/types.ts
@@ -0,0 +1,41 @@
+export type Multimedia = {
+  url: string;
+  format: string;
+  height: number;
+  width: number;
+  type: string;
+  subtype: string;
+  caption: string;
+  copyright: string;
+};
+
+export type Article = {
+  section: string;
+  subsection: string;
+  title: string;
+  abstract: string;
+  url: string;
+  uri: string;
+  byline: string;
+  item_type: string;
+  updated_date: string;
+  created_date: string;
+  published_date: string;
+  material_type_facet: string;
+  kicker: string;
+  des_facet: string[];
+  org_facet: string[];
+  per_facet: string[];
+  geo_facet: string[];
+  multimedia: Multimedia[];
+  short_url: string;
+  addClass: string | undefined;
+};
+
+export type Bookmark = {
+  id: string,
+  date?: Article["published_date"],
+  title?: Article["title"],
+  summary?: Article["abstract"],
+  url?: Article["url"],
+};
