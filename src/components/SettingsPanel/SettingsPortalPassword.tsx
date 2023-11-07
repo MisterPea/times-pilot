fb3e73e900aada60d63a0c79b2f3f3ef4985c38b
MisterPea <pea.mail.list@gmail.com>
2023-11-06 20:45:18 -0500

    Initial commit

---
+++ src/components/SettingsPanel/SettingsPortalPassword.tsx
@@ -0,0 +1,99 @@
+import { useContext, useState } from "react";
+import PrimarySecondaryButtonsHTML from "../PrimarySecondaryButtonsHTML/PrimarySecondaryButtonsHTML";
+import TextInput from "../TextInput/TextInput";
+import TextInputStaticMock from "./SettingsInputLabel";
+import SettingsPortalBlank from "./SettingsPortalBlank";
+import { AuthContext } from "../../db/Auth";
+import ErrorWarn from "../ErrorWarn/ErrorWarn";
+
+interface SettingsPortalEmailProps {
+  backCallback: () => void;
+}
+
+export default function SettingsPortalPassword({ backCallback }: SettingsPortalEmailProps) {
+  const { updateUserPassword } = useContext(AuthContext);
+  const [currValidPW, setCurrValidPW] = useState<boolean>(false);
+  const [newValidPW1, setNewValidPW1] = useState<boolean>(false);
+  const [newValidPW2, setNewValidPW2] = useState<boolean>(false);
+
+  const [currPassword, setCurrPassword] = useState<string>('');
+  const [newPassword1, setNewPassword1] = useState<string>('');
+  const [newPassword2, setNewPassword2] = useState<string>('');
+
+  const [isError, setIsError] = useState<boolean>(false);
+  const [errorMsg, setErrorMsg] = useState<string>('');
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+
+  async function handleSubmitNewUsername() {
+    if (updateUserPassword) {
+      setIsSubmitted(true);
+      const isSuccess = await updateUserPassword(newPassword1, currPassword);
+      if (!isSuccess.success) {
+        setIsError(true);
+        setErrorMsg(isSuccess.message);
+        console.log(isSuccess.message)
+        setIsSubmitted(false);
+      } else {
+        handleBackNav()
+      }
+    }
+  }
+
+  function handleBackNav() {
+    backCallback();
+  }
+
+  const createErrorMsg = (message: string) => {
+    if (message === 'auth/wrong-password') {
+      return 'It looks like you current password is incorrect';
+    }
+    return 'There Was An Issue Changing Your Password';
+  };
+
+  function resetError() {
+    setIsError(false);
+  }
+  console.log({currValidPW, newValidPW1, newValidPW2})
+  return (
+    <SettingsPortalBlank headline="Let's Change Your Password." backCallback={handleBackNav}>
+      <ErrorWarn isError={isError} errorMsg={createErrorMsg(errorMsg)} setIsError={resetError} watchArray={[currPassword, newPassword1, newPassword2]} />
+      <div className="settings_portal--input_wrap">
+        <div className="settings_portal-input_element">
+          <TextInput
+            label="Current Password"
+            type="password"
+            regexTest="password"
+            parentSetState={setCurrPassword}
+            validInputCallback={setCurrValidPW} />
+        </div>
+        <div className="settings_portal-input_bottom_wrap">
+          <div className="settings_portal-input_element">
+            <TextInput
+              label="New Password"
+              type="password"
+              regexTest="password"
+              parentSetState={setNewPassword1}
+              validInputCallback={setNewValidPW1} />
+          </div>
+          <div className="settings_portal-input_element">
+            <TextInput
+              label="Confirm New Password"
+              type="password"
+              regexTest="password"
+              parentSetState={setNewPassword2}
+              validInputCallback={setNewValidPW2} />
+          </div>
+        </div>
+      </div>
+      <div className="settings_portal-button_wrap">
+        <PrimarySecondaryButtonsHTML
+          primaryLink={handleSubmitNewUsername}
+          secondaryLink={handleBackNav}
+          disabled={!(currValidPW && newValidPW1 && newValidPW2 && (newPassword1 === newPassword2))}
+          spinner={true}
+          isWorking={isSubmitted}
+        />
+      </div>
+    </SettingsPortalBlank>
+  );
+};
